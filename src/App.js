34aeafac5779d0b1ed36413dd966ed1f7cac97cb
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-29 09:51:49 -0500

    the webpage is showing the book titles, book authors, book descriptions, and a show less & more dropdown, and css is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css'
 import bookData from './book-data.json'
+import { useState } from 'react'
 
 function App() {
   return (
     <div>
       <h1>Freeshelf</h1>
-      {/* Use bookData to show books on the page*/}
+      <div className="book-details">
+      {bookData.map((book)=> (
+        <Book
+          title={book.title}
+          author={book.author}
+          description={book.shortDescription}
+          
+        />
+      ))}
+      </div>
+    </div>
+  )
+}
+
+function Book(props) {
+  const [expanded, setExpanded] = useState(false)
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <h3>{props.author}</h3>
+      <h3>{props.description}</h3>
+      <button onClick={handleClick}>
+        {expanded ? 'show less' : 'show more'}
+      </button>
+      {expanded && <p>{props.expertise}</p>}
     </div>
   )
 }
